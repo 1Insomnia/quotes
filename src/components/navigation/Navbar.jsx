992b11270e60788ea81bc8deaeaf99700a1ef69a
Jérémy <jeremy.pro.lp@gmail.com>
2023-11-22 13:21:14 +0100

    add new logo

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import logo from "./jb-logo.svg"
 
 function Navbar() {
   return (
     <div className="shadow-xl fixed top-0 left-0 w-full z-10 main_nav">
       <div className="mx-auto max-w-screen-xl px-4 flex items-center justify-between h-16">
-        <Link to="/" className="h-8 w-8">
-          <img src="/logo.png" alt="logo" />
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            width={48}
+            height={48}
+            className="h-12 w-12"
+          />
         </Link>
         <div>Menu</div>
       </div>
